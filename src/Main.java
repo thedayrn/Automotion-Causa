f9f939d2baee917b6b37facfca825d5885caa376
thedayrn <thedabisbored@gmail.com>
2026-08-17 22:09:10 -0500

    Arraylist con metodo en main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-
+import java.util.Arraylist;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hola causas");
+        ArrayList<Vehiculo> inventario = new Arraylist<>();
+        mostrarLista (inventario);
+    }
+    public static void mostrarLista (Arraylist<Vehiculo>inventario) {
+        System.out.println ("Esta es la array del inventario")
     }
 }
