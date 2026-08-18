7d45b4d2a732b740f16595d036d32aa94b65bbe3
JonatanElia <jonatanceballosc3@gmail.com>
2026-08-18 18:35:25 -0500

    Correcciones main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import java.util.Arraylist;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hola causas");
-        ArrayList<Vehiculo> inventario = new Arraylist<>();
+        ArrayList<Vehiculo> inventario = new ArrayList<>();
         mostrarLista (inventario);
     }
-    public static void mostrarLista (Arraylist<Vehiculo>inventario) {
-        System.out.println ("Esta es la array del inventario")
+    public static void mostrarLista (ArrayList<Vehiculo>inventario) {
+        System.out.println ("Esta es la array del inventario");
     }
 }
